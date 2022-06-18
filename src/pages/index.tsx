9e65c1b8918ec8ac83998fdb904ef11d5319f5ca
bbakjae <qkrwogus0724@naver.com>
2022-06-19 01:22:53 +0900

    introduction shape

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,7 @@ import logo from '../../public/logo.png';
 import twitter from '../../public/twitter.png';
 import github from '../../public/github.png';
 import insta from '../../public/insta.png';
-import vercel from '../../public/twit.svg';
-
+import profile from '../../public/test.jpg';
 const HomePage = () => {
   return (
     <div className='main-page'>
@@ -26,6 +25,20 @@ const HomePage = () => {
               <img src={insta.src}></img>
             </a>
           </div>
+        </div>
+      </div>
+      <div className='intro-box'>
+        <div className='intro-text'>
+          Hi 👋, <br />
+          My Name is <span className='name-gradient'>BBakJae</span> <br />
+          I'm a Server Developer
+        </div>
+        <img src={profile.src} className='intro-img' />
+      </div>
+      <div className='stack'>
+        <div className='stack-title'>
+          My Stack List
+          <div className='stack-description'>Technologies I've been working with recently</div>
         </div>
       </div>
     </div>
